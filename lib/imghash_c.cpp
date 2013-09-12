72c0fab46d47f890e9eae0780e84807c72291a40
shutty <grebennikov.roman@gmail.com>
2013-09-12 16:57:40 +0400

    add new functions to c interface

--- lib/imghash_c.cpp
+++ lib/imghash_c.cpp
@@ -1,10 +1,32 @@
 #include "imghash_c.h"
 
-unsigned long long imghash_c(char *buffer, unsigned int size)
+unsigned long long imghash_buffer(char *buffer, unsigned int size)
 {
     imghash::Source source(buffer, size);
     imghash::Hasher *hasher = new imghash::SimpleHasher();
     imghash::Hash hash = hasher->hash(source);
     delete hasher;
     return hash.toLongLong();
+}
+
+
+unsigned long long imghash_file(char *fileName)
+{
+    imghash::Source source;
+    source.loadFile(fileName);
+    imghash::Hasher *hasher = new imghash::SimpleHasher();
+    imghash::Hash hash = hasher->hash(source);
+    delete hasher;
+    return hash.toLongLong();
+}
+
+
+const char *imghash_file_hex(char *fileName)
+{
+    imghash::Source source;
+    source.loadFile(fileName);
+    imghash::Hasher *hasher = new imghash::SimpleHasher();
+    imghash::Hash hash = hasher->hash(source);
+    delete hasher;
+    return hash.toHex().c_str();
 }
--- lib/imghash_c.h
+++
@@ -1,8 +0,0 @@
-#ifndef IMGHASH_C_H
-#define IMGHASH_C_H
-
-#include "simplehasher.h"
-
-unsigned long long int imghash_c(char *buffer, unsigned int size);
-
-#endif
