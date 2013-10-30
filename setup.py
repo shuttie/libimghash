96470748ffcfcd883c78117ef7c35265c0f8b49e
shutty <grebennikov.roman@gmail.com>
2013-10-30 15:11:06 +0400

    move setup.py to root folter

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup, Extension
 
 imghash_module = Extension('_imghash',
 		    define_macros = [('MAGICKCORE_QUANTUM_DEPTH','8'),('MAGICKCORE_HDRI_ENABLE','0')],
-		    include_dirs = ['../include','/usr/include/ImageMagick', '/usr/include/ImageMagick-6'],
+		    include_dirs = ['include','/usr/include/ImageMagick', '/usr/include/ImageMagick-6'],
 		    libraries = ['imghash'],
-                    sources = ['imghash_wrap.cpp','../lib/imghash_c.cpp'])
+                    sources = ['python/imghash_wrap.cpp','lib/imghash_c.cpp'])
 
 setup(name='imghash',
       version='0.1.1',
