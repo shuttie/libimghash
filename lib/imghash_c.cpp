2236afa3acc3f45383a8fa450e43613b96432f61
shutty <grebennikov.roman@gmail.com>
2013-10-30 15:07:02 +0400

    wrap all C interface code with try-catch block

--- lib/imghash_c.cpp
+++ lib/imghash_c.cpp
@@ -2,31 +2,46 @@
 
 unsigned long long imghash_buffer(char *buffer, unsigned int size)
 {
-    imghash::Source source(buffer, size);
-    imghash::Hasher *hasher = new imghash::SimpleHasher();
-    imghash::Hash hash = hasher->hash(source);
-    delete hasher;
-    return hash.toLongLong();
+    try {
+        imghash::Source source(buffer, size);
+        imghash::Hasher *hasher = new imghash::SimpleHasher();
+        imghash::Hash hash = hasher->hash(source);
+        delete hasher;
+        return hash.toLongLong();
+    } catch (Magick::Error e) {
+        printf("oops, cannot calculate hash: %s\n", e.what());
+        return 0;
+    }
 }
 
 
 unsigned long long imghash_file(char *fileName)
 {
-    imghash::Source source;
-    source.loadFile(fileName);
-    imghash::Hasher *hasher = new imghash::SimpleHasher();
-    imghash::Hash hash = hasher->hash(source);
-    delete hasher;
-    return hash.toLongLong();
+    try {
+        imghash::Source source;
+        source.loadFile(fileName);
+        imghash::Hasher *hasher = new imghash::SimpleHasher();
+        imghash::Hash hash = hasher->hash(source);
+        delete hasher;
+        return hash.toLongLong();
+    } catch (Magick::Error e) {
+        printf("oops, cannot calculate hash: %s\n", e.what());
+        return 0;
+    }
 }
 
 
 const char *imghash_file_hex(char *fileName)
 {
-    imghash::Source source;
-    source.loadFile(fileName);
-    imghash::Hasher *hasher = new imghash::SimpleHasher();
-    imghash::Hash hash = hasher->hash(source);
-    delete hasher;
-    return hash.toHex().c_str();
+    try {
+        imghash::Source source;
+        source.loadFile(fileName);
+        imghash::Hasher *hasher = new imghash::SimpleHasher();
+        imghash::Hash hash = hasher->hash(source);
+        delete hasher;
+        return hash.toHex().c_str();
+    } catch (Magick::Error e) {
+        printf("oops, cannot calculate hash: %s\n", e.what());
+        return 0;
+    }
 }
--- python/imghash.i
+++ python/imghash.i
@@ -4,6 +4,6 @@
 #include "imghash_c.h"
 %}
 
-extern unsigned long long imghash_buffer(char* buffer, unsigned int size);
+extern unsigned long long imghash_buffer(char* buffer, int bufferSize);
 extern unsigned long long imghash_file(char* fileName);
 extern char* imghash_file_hex(char* fileName);
